839d05cf54a4ea8d739965922185ec2e5bd8c96b
ahmed@trytrue.com <ahmed@trytrue.com>
2021-04-22 21:13:44 +0300

    Covered ConversationMapper with unit tests

--- server/src/test/kotlin/com/aabdelhady/stomp/messenger/test/DummyModelUtil.kt
+++ server/src/test/kotlin/com/aabdelhady/stomp/messenger/test/DummyModelUtil.kt
@@ -1,14 +1,18 @@
 package com.aabdelhady.stomp.messenger.test
 
 import com.aabdelhady.stomp.messenger.feature.conversation.model.Conversation
+import com.aabdelhady.stomp.messenger.feature.message.core.model.Message
 import com.aabdelhady.stomp.messenger.feature.user.model.AuthProvider
 import com.aabdelhady.stomp.messenger.feature.user.model.User
+import java.time.Instant
 import java.util.*
 
-fun createDummyUser(authProvider: AuthProvider = AuthProvider.GOOGLE,
-                        firstName: String = randomFirstName(), lastName: String = randomLastName(),
-                        email: String = "$firstName.$lastName@gmail.com".toLowerCase()) =
-        User(UUID.randomUUID().toString(), authProvider, firstName, lastName, email).apply { id = randomId() }
+fun createDummyUser(
+    authProvider: AuthProvider = AuthProvider.GOOGLE,
+    firstName: String = randomFirstName(), lastName: String = randomLastName(),
+    email: String = "$firstName.$lastName@gmail.com".toLowerCase()
+) =
+    User(UUID.randomUUID().toString(), authProvider, firstName, lastName, email).apply { id = randomId() }
 
 fun createDummyUsers(count: Int): List<User> {
     val users = mutableListOf<User>()
@@ -17,3 +21,8 @@ fun createDummyUsers(count: Int): List<User> {
 }
 
 fun createDummyConversation(participants: List<User> = createDummyUsers(2)) = Conversation(participants).apply { id = randomId() }
+
+fun createDummyMessage(conversation: Conversation, sender: User, text: String = randomSentence()) = Message(conversation, sender, text).apply {
+    id = randomId()
+    sent = Instant.now()
+}
